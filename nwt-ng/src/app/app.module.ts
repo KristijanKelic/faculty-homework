13f31cdd37fe32ea03c61fdd129258f123f1d270
Kristijan <thektheking@gmail.com>
2018-12-30 17:27:09 +0100

    few more components created

--- nwt-ng/src/app/app.module.ts
+++ nwt-ng/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { FavoritesComponent } from './components/favorites/favorites.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductListComponent,
+    ProductComponent,
+    ProductDetailComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
